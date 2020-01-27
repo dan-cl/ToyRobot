b024358d138bbdbccfcec81b581f7076f7278974
Dan-Cl <Dan.Clissold@protonmail.com>
2020-01-27 13:37:46 +1100

    Create basic classes

---
+++ ToyRobot.App/Map/MapCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToyRobot.App.Map
+{
+    public class MapCommandValidator
+    {
+
+    }
+}
---
+++ ToyRobot.App/Robot/Robot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ToyRobot.App.Robot
+{
+    public class Robot
+    {
+        private int[][] _position;
+        private string _Heading;
+
+        public int[][] Position { get; set; }
+        public string Heading { get; set; }
+
+    }
+}
