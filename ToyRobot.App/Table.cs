51a9a101a4521a9c154308c8da345e9fcee9d601
Dan-Cl <Dan.Clissold@protonmail.com>
2020-01-28 13:23:38 +1100

    Write and test move method

--- ToyRobot.App/Table.cs
+++ ToyRobot.App/Table.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 
@@ -8,14 +9,30 @@ namespace ToyRobot.App
     {
         private readonly int _tableSize;
 
+        public delegate bool MoveMethodDelegate(IRobot robot);
+
+        public Dictionary<string, Delegate> MoveMethods;
+
         public Table()
         {
-            _tableSize = 5;
+            _tableSize = TableSizeConstants.TableSize;
+
+            MoveMethods = new Dictionary<string, Delegate>()
+            {
+                {Constants.North, new MoveMethodDelegate(MoveNorth) },
+                {Constants.East, new MoveMethodDelegate(MoveEast) },
+                {Constants.South, new MoveMethodDelegate(MoveSouth) },
+                {Constants.West, new MoveMethodDelegate(MoveWest) },
+            };
         }
 
-        public bool Move(Robot robot)
+        public bool Move(IRobot robot)
         {
-            return true;
+            if(!ValidPosition(robot.Position)) return false;
+
+            if (!ValidHeading(robot.Heading, out var moveMethodDelegate)) return false;
+
+            return moveMethodDelegate(robot);
         }
 
         public bool Place(int[] position, IRobot robot)
@@ -27,9 +44,64 @@ namespace ToyRobot.App
             return true;
         }
          
+
+
         private bool ValidPosition(int[] position)
         {
             return position.All(value => value >= 0 && _tableSize >= value);
+        }
+
+        private bool ValidHeading(string robotHeading, out MoveMethodDelegate moveMethodDelegate)
+        {
+            foreach (var headingString in Constants.HeadingStrings)
+            {
+                if (robotHeading != headingString) continue;
+
+                moveMethodDelegate = MoveMethods[headingString] as MoveMethodDelegate;
+                return true;
+            }
+
+            moveMethodDelegate = null;
+            return false;
+        }
+
+        private bool MoveNorth(IRobot robot)
+        {
+            var axis = 1;
+            var velocity = 1;
+            return ChangeCoordinate(robot, axis, velocity);
+        }
+
+        private bool MoveEast(IRobot robot)
+        {
+            var axis = 0;
+            var velocity = 1;
+            return ChangeCoordinate(robot, axis, velocity);
+        }
+
+        private bool MoveSouth(IRobot robot)
+        {
+            var axis = 1;
+            var velocity = -1;
+            return ChangeCoordinate(robot, axis, velocity);
+        }
+
+        private bool MoveWest(IRobot robot)
+        {
+            var axis = 0;
+            var velocity = -1;
+            return ChangeCoordinate(robot, axis, velocity);
+        }
+
+        private bool ChangeCoordinate(IRobot robot, int axis, int velocity)
+        {
+            var newPosition = (int[])robot.Position.Clone();
+            newPosition[axis] += velocity;
+
+            if (!ValidPosition(newPosition)) return false;
+
+            robot.Position[axis] += velocity;
+            return true;
         }
     }
 }
